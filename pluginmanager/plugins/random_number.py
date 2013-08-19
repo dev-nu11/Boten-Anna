d50e623485763d2f02724e5eaaa2214309ec32ee
Jonas Platte <jonasplatte@myopera.com>
2013-08-19 20:53:00 +0200

    Added random number plugin

---
+++ pluginmanager/plugins/random_number.py
@@ -0,0 +1,27 @@
+from pluginmanager.pluginmanager import register_plugin, plugin
+import random
+
+class RandomNumber(plugin):
+
+  name = 'Random number'
+  
+  # 0: Match on Private Messages | 1: Match on Group Chat
+  permissions = [True,True]   
+
+  def plugin_init():
+    pass
+
+  def match():
+    return '^!(random-number)($|\s)'
+
+  def send_message(message,match,nick):
+    msg = message.split(' ')[1:]
+    if len(msg) == 2 and msg[0].isdecimal() and msg[1].isdecimal():
+      return str(random.randint(int(msg[0]), int(msg[1])))
+    else:
+      return 'Usage:\n\t!random-number <range-begin> <range-end>'
+
+  def help():
+    return 'Random number plugin\nUsage: !random-number <range-begin> <range-end>'
+      
+register_plugin(RandomNumber)
